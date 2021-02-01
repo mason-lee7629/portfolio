8139fe761e4727d501c8c96acbb6d75d2f40b543
Mason <masonlee7629@gmail.com>
2021-02-02 00:24:28 +0900

    add navigation page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,26 @@
-// pagesize function
-function resizePage() {
-  let vwidth = $(window).innerWidth();
-  let vheight = $(window).height();
-  $(".onepage").css({ width: vwidth, height: vheight });
-}
-// scrollUp function
-function scrollUp() {
-  let vheight = $(window).innerWidth();
-  $("html, body").animate(
-    {
-      scrollTop: (Math.ceil($(window).scrollTop() / vheight) - 1) * vheight,
-    },
-    500
-  );
-}
-// scrollDown function
-function scrollDown() {
-  let vheight = $(window).innerWidth();
-  $("html, body").animate(
-    {
-      scrollTop: (Math.ceil($(window).scrollTop() / vheight) + 1) * vheight,
-    },
-    500
-  );
+let scrollSpeed = 700;
+
+function scrollPage(obj) {
+  if (!obj) {
+    $('html, body').animate({ scrollTop: 0 }, scrollSpeed);
+  } else {
+    let posTop = $(obj).offset().top;
+    $('html, body').animate({ scrollTop: posTop }, scrollSpeed);
+  }
 }
 
+$('.nav-circle').click(function () {
+  let direction = $(this).attr('href');
+  scrollPage(direction);
+  return false;
+});
+
 $(document).ready(function () {
-  resizePage();
   // main-text의 text color 변경 함수
   function changeHeaderColorTimer() {
-    let colorCode = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-    $("#changeColor").css("color", colorCode);
+    let colorCode = '#' + Math.round(Math.random() * 0xffffff).toString(16);
+    $('#changeColor').css('color', colorCode);
   }
   // main-text의 text color 변경 실행함수
   setInterval(changeHeaderColorTimer, 1500);
-});
-
-$(document).keydown(function (event) {
-  if (event.keyCode == 37 || event.keyCode == 38) {
-    scrollUp();
-  } else if (event.keyCode == 39 || event.keyCode == 40) {
-    scrollDown();
-  }
-});
-
-$(window).resize(function () {
-  resizePage();
 });
